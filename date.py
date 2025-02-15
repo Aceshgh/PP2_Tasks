7f8766ecd0afe9dfc1204ae7f6b378db6f72c60a
Aceshgh <99142250+Aceshgh@users.noreply.github.com>
2025-02-15 11:09:20 +0500

    Add files via upload

---
+++ date.py
@@ -0,0 +1,38 @@
+#1
+#from datetime import datetime,timedelta
+
+#timenow = datetime.today()
+#timecurr =timenow -timedelta(days=5)
+#print("current time",timenow.date())
+#print("5 days ago",timecurr.date())
+
+
+
+#2
+#from datetime import datetime, timedelta
+
+#today =datetime.today().date()
+#yesterd = today -timedelta(days=1)
+#tomorrw = today +timedelta(days=1)
+
+
+##print("yesterday ", yesterd)
+#print("today ", today)
+#print("tomorrow ", tomorrw)
+
+
+#3
+#from datetime import datetime
+
+#now =datetime.now().replace(microsecond=0)
+#print("Current time without microseconds ",now)
+
+
+#4
+#from datetime import datetime
+
+#date1 =datetime(2024,2,10,12,0,0)
+#date2 =datetime(2024,2,15,14,30, 0)
+#difference =(date2-date1).total_seconds()
+
+#print("difference in seconds ",difference)
